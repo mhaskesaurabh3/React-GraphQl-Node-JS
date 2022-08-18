e3763e33edbcc1b650c425a002f4a8070cc1742a
Saurabh Mhaske <mhaskesaurabh3@gmail.com>
2022-08-18 14:06:58 +0530

    Types and Resolvers created for Graphql API

---
+++ FakeData.js
@@ -0,0 +1,34 @@
+const UserList = [
+  {
+    id: 1,
+    name: 'John',
+    age: 20,
+    nationality: 'American',
+  },
+  {
+    id: 2,
+    name: 'Pedro',
+    age: 20,
+    nationality: 'Irish',
+  },
+  {
+    id: 3,
+    name: 'Sarah',
+    age: 25,
+    nationality: 'Indian',
+  },
+  {
+    id: 4,
+    name: 'Rafe',
+    age: 60,
+    nationality: 'France',
+  },
+  {
+    id: 5,
+    name: 'Kelly',
+    age: 5,
+    nationality: 'Russian',
+  },
+];
+
+module.exports = { UserList };
